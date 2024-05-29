f9067534e912548e486acb6e4d00a1701290b421
b3tinho007 <0000110341379XSP@al.educacao.sp.gov.br>
2024-05-29 18:30:38 -0300

    alterações

---
+++ sketch.js
@@ -0,0 +1,103 @@
+//variáveis da bolinha
+let xBolinha = 100;
+let yBolinha = 200;
+let diametro = 22;
+let raio = diametro / 2;
+
+//velocidade da bolinha
+let velocidadeXBolinha = 6;
+let velocidadeYBolinha = 6;
+
+//variáveis da raquete
+let xRaquete = 5;
+let yRaquete = 150;
+let raqueteComprimento = 10;
+let raqueteAltura = 90;
+
+let colidiu = false;
+
+//placar
+let meusPontos = 0
+let pontosOponente = 0
+
+let xRaqueteOponente = 585
+let yRaqueteOponente = 150
+
+let velocidadeYOponente;
+
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background("black");
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisaoBorda();
+  mostraRaquete(xRaquete, yRaquete);
+  mostraRaquete(xRaqueteOponente,yRaqueteOponente)
+  movimentaMinhaRaquete();
+  verificaColisaoRaquete(xRaquete, yRaquete);
+  verificaColisaoRaquete(xRaqueteOponente, yRaqueteOponente);
+  movimentaRaqueteDoOponente();
+  incluiPlacar();
+  marcaPontos();
+}
+function mostraBolinha() {
+  circle(xBolinha, yBolinha, diametro);
+}
+
+function movimentaBolinha() {
+  xBolinha += velocidadeXBolinha;
+  yBolinha += velocidadeYBolinha;
+}
+
+function verificaColisaoBorda() {
+  if (xBolinha + raio > width || xBolinha - raio < 0) {
+    velocidadeXBolinha *= -1;
+  }
+  if (yBolinha + raio > height || yBolinha - raio < 0) {
+    velocidadeYBolinha *= -1;
+  }
+}
+
+function mostraRaquete(x, y) {
+  rect(x, y, raqueteComprimento, raqueteAltura);
+}
+
+function movimentaMinhaRaquete() {
+  if(keyIsDown(UP_ARROW)) {
+    yRaquete -= 10;
+  }
+  if(keyIsDown(DOWN_ARROW)) {
+    yRaquete += 10;
+  }
+}
+
+function verificaColisaoRaquete(x, y) {
+  colidiu = collideRectCircle(x. v, yRaquete, raqueteComprimento, raqueteAltura, xBolinha, yBolinha, raio);
+  if(colidiu) {
+    velocidadeXBolinha *= -1;
+ }
+ }
+
+function  movimentaRaqueteDoOponente(){
+  velocidadeYOponente = yBolinha -yRaqueteOponente -
+raqueteComprimento / 2 -30
+  yRaqueteOponente += velocidadeYOponente
+}
+function incluiPlacar() {
+  fill('pink')
+  text(meusPontos, 278, 26)
+  text(pontosOponente, 321, 26) 
+  
+}
+function marcaPontos(){
+  if (xBolinha > 590) {
+    meusPontos += 1;
+  }
+  if (xBolinha < 10) {
+    pontosDoOponente += 1;
+  }
+
+}
